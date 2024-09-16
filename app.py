1f844e99db3e78fcedcd2a21072691acfbb12085
Lucas <lucasfrias878@gmail.com>
2024-09-15 19:55:59 -0500

    Try again

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
 from flask import Flask, url_for, render_template, request, url_for, redirect, send_file
 from flask_login import (
-    LoginManager, 
+    LoginManager,
     current_user,
     login_required,
-    login_user, 
+    login_user,
     logout_user,
 )
 #Several different methods for database/Oauth logins
@@ -16,51 +16,17 @@ import os, time, json, requests, random, string
 import os
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return render_template("editor.html")
-
-@app.route('/webapp')
-def test():
-    return render_template("webapp.html")
-GOOGLECLIENTID = os.environ.get("GOOGLECLIENTID")
-GOOGLECLIENTSECRET = os.environ.get("GOOGLECLIENTSECRET")
-print(GOOGLECLIENTID)
-print(GOOGLECLIENTSECRET)
-GOOGLEDISCOVERYURL = ("https://accounts.google.com/.well-known/openid-configuration")
-
 #Now, let's get an OAuth2 client running
-client = WebApplicationClient(GOOGLECLIENTID)
 
 #Initialise flask app, which will be accessed by other apps
 app = Flask(__name__)
 
-#Initialise login manager
-loginmanager = LoginManager()
-loginmanager.init_app(app)
 
-#########################
-# Google OAuth Handler  #
-#########################
-def getGoogleProviderCFG(): #This returns the current authorisation end point for Google through an open updated page.
-    #It returns it as a JSON object that is accessed in /login
-    return requests.get(GOOGLEDISCOVERYURL).json()
-#########################
-# Loginmanager Requests #
-#########################
-
-@loginmanager.user_loader #Returns user data
-def load_user(user):
-    return User.get(user)
 
 ########################
 # Flask Page Requests #
 #######################
 
-###### Home index /
-@app.route('/') #Returns home HTML file
-def index():
-        return render_template("editor.html")
 
 
 ##############################
@@ -119,6 +85,11 @@ def delete():
 @app.route("/make.mp3")
 def make():
     return send_file("static/make.mp3")
+
+
+@app.route('/')
+def hello_world():
+    return render_template("main.html")
 
 if __name__ == "__main__":
     app.run(ssl_context="adhoc", host="0.0.0.0", port=8000)
